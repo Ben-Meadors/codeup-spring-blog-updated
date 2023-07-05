75021f75d9aa5f9e4107c37d8fbfb07aa3173425
Ben Meadors <ben.p.meadors@gmail.com>
2023-07-05 10:38:30 -0400

    forgot to commit these maybe

--- src/main/java/com/codeup/codeupspringblog2/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog2/controllers/PostController.java
@@ -1,18 +1,37 @@
 package com.codeup.codeupspringblog2.controllers;
 
+import com.codeup.codeupspringblog2.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(path = "/posts")
+import java.util.ArrayList;
+import java.util.List;
 
+@Controller
 public class PostController {
 
-    @GetMapping()
-    @ResponseBody
-    public String viewPosts() {
-        return "<h1>This is the posts index page</h1>";
+    @GetMapping("/post")
+    public String post(Model model){
+        model.addAttribute("post", new Post("Hello", "Hello World"));
+        return "/posts/index";
     }
+
+    @GetMapping("/posts")
+    public String posts(Model model){
+        List<Post> posts= new ArrayList<>();
+        posts.add(new Post("Hello", "HelloWorld"));
+        posts.add(new Post("Hello 2", "Hello Again"));
+        model.addAttribute("posts", posts);
+        return "/posts/show";
+    }
+
+
+//    @GetMapping()
+//    @ResponseBody
+//    public String viewPosts() {
+//        return "<h1>This is the posts index page</h1>";
+//    }
 
     @GetMapping("/{id}")
     @ResponseBody
---
+++ src/main/java/com/codeup/codeupspringblog2/models/Post.java
@@ -0,0 +1,16 @@
+package com.codeup.codeupspringblog2.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    private String title;
+    private String body;
+}
